be4bec751063a0eaeaa09793eeaf88061d8dd147
FederAndInk <federandink@gmail.com>
2017-10-25 08:53:13 +0200

    login form

---
+++ view/login_flux.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<!-- newLogin et mdp -->
+<form action="../controler/login_flux.ctrl.php" method="post">
+	<label>Nom d'utilisateur : </label> <input type="text">
+	<label>Mot de passe : </label> <input type="password">
+	<input type="submit" value="Nouveau Compte">
+	<input type="submit" value="Se connecter">
+</form>
+<?php 
+
+?>
+</body>
+</html>
